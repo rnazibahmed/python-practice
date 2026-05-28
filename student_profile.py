f55d5585735528d8eec3ccebd17a28c7bbf34724
rnazibahmed <rnazib.ahmed@gmail.com>
2026-05-28 13:56:13 -0400

    Add student profile practice

---
+++ student_profile.py
@@ -0,0 +1,11 @@
+name = "Rezoan"
+goal = "AI/ML Engineer"
+hours_per_day = 4
+
+print("name:", name)
+print("Career Gaol:", goal)
+print("Daily Study Hours:", hours_per_day)
+
+
+weekly_hours = hours_per_day * 7
+print("Weekly Study Hours:", weekly_hours)
