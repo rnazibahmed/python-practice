0617c8050e4d623ee75352ea381e6e0e6c7342f0
rnazibahmed <rnazib.ahmed@gmail.com>
2026-05-31 21:49:04 -0400

    corrected logic in second largest

--- second_largest.py
+++ second_largest.py
@@ -7,13 +7,20 @@ for i in range(num):
     temp = int(input("Please enter a number: "))
     numbers.append(temp)
 
-largest = numbers[0]
-second_largest = numbers[0]
+if numbers[0] > numbers[1]:
+    largest = numbers[0]
+    second_largest = numbers[1]
+else:
+    second_largest = numbers[0]
+    largest = numbers[1]
 
-for number in numbers:
+for number in numbers[2:]:
     if number > largest:
         second_largest = largest
         largest = number
+    elif number > second_largest:
+        second_largest = number
+        
 
 print("Numbers Entered: ", numbers)
 print("Largest: ", largest)
